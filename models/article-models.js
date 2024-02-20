8b590591d75536eee62fd22e98197ea299512b0d
qmumtaz <q.mumtaz@hotmail.com>
2024-02-20 09:44:39 +0000

    completed fully sprint 4

--- models/article-models.js
+++ models/article-models.js
@@ -1,5 +1,10 @@
 const db = require('../db/connection');
 
 exports.selectArticleById = (id) => {
-    return db.query('SELECT * FROM articles WHERE article_id = $1', [id]);
+    return db.query('SELECT * FROM articles WHERE article_id = $1', [id]).then(({rows}) => {
+        if (rows.length === 0) {
+            return Promise.reject({ status: 400, msg: 'Bad request' });
+        }
+        return rows[0];
+    });
 }

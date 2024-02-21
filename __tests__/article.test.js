7a2c03a688811a81e5b0db79c37a9c7cccd3c1cc
qmumtaz <q.mumtaz@hotmail.com>
2024-02-21 11:06:38 +0000

    completed sprint 12

--- __tests__/article.test.js
+++ __tests__/article.test.js
@@ -25,6 +25,7 @@ describe("/api/articles", () => {
           votes: 100,
           article_img_url:
             "https://images.pexels.com/photos/158651/news-newsletter-newspaper-information-158651.jpeg?w=700&h=700",
+            comment_count: "11"
         });
       });
   });
--- models/article-models.js
+++ models/article-models.js
@@ -1,7 +1,17 @@
 const db = require('../db/connection');
 
 exports.selectArticleById = (id) => {
-    return db.query('SELECT * FROM articles WHERE article_id = $1', [id]).then(({rows}) => {
+    const query = `
+    SELECT 
+    articles.*,
+    COUNT(comments.comment_id) AS comment_count
+    FROM articles 
+    LEFT JOIN comments ON articles.article_id = comments.article_id
+    WHERE articles.article_id = $1
+    GROUP BY articles.article_id;
+    `;
+
+    return db.query(query, [id]).then(({rows}) => {
         if (rows.length === 0) {
             return Promise.reject({ status: 404, msg: 'Not Found' });
         }

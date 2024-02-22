b870b631567529c5535d4331858633da59262ae1
qmumtaz <q.mumtaz@hotmail.com>
2024-02-22 11:09:00 +0000

    updated package.json

--- db/connection.js
+++ db/connection.js
@@ -1,13 +1,6 @@
 const { Pool } = require('pg');
 const ENV = process.env.NODE_ENV || 'development';
 
-const config = {};
-
-if (ENV === 'production') {
-  config.connectionString = process.env.DATABASE_URL;
-  config.max = 2;
-}
-
 require('dotenv').config({
   path: `${__dirname}/../.env.${ENV}`,
 });
@@ -15,5 +8,18 @@ require('dotenv').config({
 if (!process.env.PGDATABASE && !process.env.DATABASE_URL) {
   throw new Error('PGDATABASE or DATABASE_URL not set');
 }
+
+const config = {};
+
+if (ENV === 'production') {
+  config.connectionString = process.env.DATABASE_URL;
+  config.max = 2;
+}
+
+
+
+
+
+
 
 module.exports = new Pool();
--- listen.js
+++ listen.js
@@ -1,6 +1,6 @@
 const app = require('./app'); 
 
-const PORT = process.env.PORT || 9090;
+const { PORT = 9090 } = process.env;
 
 app.listen(PORT, () => {
   console.log(`Server is listening on port ${PORT}...`);
